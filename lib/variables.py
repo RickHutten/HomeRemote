77fabf191a513275a35e45689939793e095b1ab6
Rick Hutten <rick.hutten@gmail.com>
2015-12-14 02:40:23 +0100

    variables.py clean up

--- lib/variables.py
+++ lib/variables.py
@@ -5,16 +5,14 @@ class ServerVariables():
         self.filename = "./data/server_variables"
         # If the file does not exist, make a new one
         try:
-            # No errors if it exists
-            f = open(self.filename, "r")
-            f.close()
+            # Try to read the file
+            open(self.filename, "r").close()
         except IOError:
-            # File does not exist
-            f = open(self.filename, "w")
-            f.close()
+            # File does not exist, create new
+            open(self.filename, "w").close()
 
     def get(self, key):
-        if type(key) != type(""):
+        if type(key) != str:
             raise ValueError("Argument should be string not of type: " + str(type(key)))
         key = key.strip()
         f = open(self.filename, "r")
@@ -23,13 +21,13 @@ class ServerVariables():
             if linesplit[0].strip() == key:
                 f.close()
                 return literal_eval(linesplit[1].strip())
-        # Item not found
+        # Key not found
         f.close()
-        raise LookupError("Item not found: " + key)
+        raise LookupError("Key not found: " + key)
 
     def put(self, key, value):
-        # Puts variable in file. Overwrites if already present.
-        if type(key) != type(""):
+        # Puts key with corresponding value in file. Overwrites if already present.
+        if type(key) != str:
             raise ValueError("First argument should be string not of type: " + str(type(key)))
         if type(value) not in [int, float, str, list, dict, tuple, bool, type(None)]:
             raise ValueError("Value type not supported: " + str(type(value)))
@@ -39,36 +37,37 @@ class ServerVariables():
         for i, line in enumerate(f):
             linesplit = line.split("=")
             if linesplit[0].strip() == key:
-                # Line already exists
+                # Key already exists
                 f.close()
                 line_no = i
                 break
         else:
-            # Line does not exists
+            # Key does not exists
             f.close()
             # Append to file
             f = open(self.filename, "a")
-            if type(value) == type(""):
+            if type(value) == str:
                 f.write(key + " = '" + str(value) + "'\n")
             else:
                 f.write(key + " = " + str(value) + "\n")
             f.close()
             return
-        # Line already exists
+        # Key already exists
         f = open(self.filename, "r")
         lines = f.readlines()
-        if type(value) == type(""):
+        if type(value) == str:
             lines[line_no] = key + " = '" + str(value) + "'\n"
         else:
             lines[line_no] = key + " = " + str(value) + "\n"
         f.close()
+        # Overwrite new lines to file
         f = open(self.filename, "w")
         f.writelines(lines)
         f.close()
 
     def contains(self, key):
         # Return True/False if file contains key
-        if type(key) != type(""):
+        if type(key) != str:
             raise ValueError("Argument should be string not of type: " + str(type(key)))
         key = key.strip()
         f = open(self.filename, "r")
